cbfad3c61a340e3b29b6c0bf72ab7945175cccf5
Aziz <azizkaraca0@gmail.com>
2023-06-15 10:30:21 +0200

    DB Connection & Auth

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
 
-dotenv.config();
 const app = express();
+dotenv.config();
+
+connectDB();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
