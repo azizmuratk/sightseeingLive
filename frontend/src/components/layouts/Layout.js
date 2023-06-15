9c493ff38d11159c29b2d08211f5298767d481d9
Aziz <azizkaraca0@gmail.com>
2023-06-15 11:49:16 +0200

    react-pages-added

---
+++ frontend/src/components/layouts/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = (props) => {
+  return (
+    <React.Fragment>
+      <Header />
+      <main style={{minHeight:'80vh'}}>{props.children}</main>
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default Layout;
